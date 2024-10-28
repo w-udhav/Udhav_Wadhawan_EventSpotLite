571812c24569b3a015a71332fa37accc9df9ffd1
w-udhav <udhavwadhawan@hotmail.com>
2024-10-29 00:48:03 +0530

    Footer is responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -26,25 +26,27 @@ export default function Footer() {
   return (
     <div className="bg-zinc-200/20 pb-20 pt-10">
       <AppTemplate>
-        <div className="w-full grid grid-cols-4 gap-2 border-b border-zinc-200/50 pb-16">
-          <div className="-mb-2 col-span-4">
+        <div className="w-full grid grid-cols-2 md:grid-cols-4 gap-2 border-b border-zinc-200/50 pb-16">
+          <div className="-mb-2 col-span-2 md:col-span-4">
             <img src={logo} alt="logo" className="w-56 -ml-10" />
           </div>
 
-          <div className="mb-10 flex items-center gap-2 col-span-4">
+          <div className="mb-10 flex items-center gap-2 col-span-2 md:col-span-4">
             <a href="https://instagram.com/w_udhav" className="">
               <img src={instagram} alt="instagram" className="w-8" />
             </a>
           </div>
 
-          <h1 className="font-medium text-2xl col-span-4 pb-2">Top events</h1>
+          <h1 className="font-medium text-2xl col-span-2 md:col-span-4 pb-2">
+            Top events
+          </h1>
 
           {eventsInCity.map((city) => (
             <p
               key={city}
               className="text-zinc-300 hover:text-zinc-500 cursor-pointer"
             >
-              Events in {city}
+              {city}
             </p>
           ))}
         </div>
